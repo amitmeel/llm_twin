0a2f8aa8aff243707302a275837d1d8ecc59ea35
Amit Meel <amit.amitmeel.meel@gmail.com>
2025-02-04 15:59:11 +0530

    added the env variable access using settings

--- code_snippets/mondb_test.py
+++ code_snippets/mondb_test.py
@@ -1,6 +1,11 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
-uri = "mongodb+srv://<db_username>:<db_password>@cluster0.v0mjb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+from src.settings import Settings
+
+settings = Settings().load_settings()
+db_username = settings.MONGODB_DATABASE_USERNAME
+db_password = settings.MONGODB_DATABASE_PASSWORD
+uri = f"mongodb+srv://{db_username}:{db_password}@cluster0.v0mjb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
 # Send a ping to confirm a successful connection
